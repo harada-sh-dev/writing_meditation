4e4717d473663d3dafb7a8e4fc5a4db879ca9806
Harada.s <harada.shigeyuki@gmail.com>
2025-12-20 20:27:55 +0900

    ビックリマーク追加

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ with open('diary.txt','a',encoding='utf-8') as file:
     file.write(date + '\n')
     entry = input('今日の日記を入力してください：')
     file.write(entry + '\n')
-print('日記は保存されました、今日も1日ご苦労さまでした！')
+print('日記は保存されました、今日も1日ご苦労さまでした！！！')
