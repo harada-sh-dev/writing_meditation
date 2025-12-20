8c25fe0969d31ea7f858c7f9b8756cfd6334835b
Harada.s <harada.shigeyuki@gmail.com>
2025-12-20 20:18:32 +0900

    初めてのコミット

---
+++ main.py
@@ -0,0 +1,6 @@
+with open('diary.txt','a',encoding='utf-8') as file:
+    date = input('今日の日付をyyyymmddの形で入力してください：')
+    file.write(date + '\n')
+    entry = input('今日の日記を入力してください：')
+    file.write(entry + '\n')
+print('日記は保存されました、今日も1日ご苦労さまでした！')
